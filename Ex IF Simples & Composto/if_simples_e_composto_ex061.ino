08d850ccdfb8e005c7d5c546c3c79cab16960021
Giulia <giuliamarzano1006@gmail.com>
2025-05-09 09:59:00 -0300

    Update if_simples_e_composto_ex061.ino

--- Ex IF Simples & Composto/if_simples_e_composto_ex061.ino
+++ Ex IF Simples & Composto/if_simples_e_composto_ex061.ino
@@ -2,33 +2,29 @@
 //Faça um programa que receba um número de usuário e exiba este
 //número, apenas se for par.
 
-
+	
 int numero = 0;
-
-
+	
+		
 void setup()
 {
   Serial.begin(9600);
+		
+  Serial.println("Digite um numero par");
+  while (!Serial.available());
+  numero = Serial.parseFloat();
+		  
+  if ( numero % 2==0){
+  Serial.println("numero par: " + String(numero));
+  Serial.println();
 }
+			 
+   else {
+   Serial.println("numero invalido");
+   Serial.println();
+}
+			
+ }
 
 void loop()
-{
-  Serial.println("Digite um numero");
-	while (!Serial.available());
-	numero = Serial.parseFloat();
-  
-  if ( numero % 2==0){
-    Serial.println("numero par: " + String(numero));
-    Serial.println();
-  }
-    else {
-   Serial.println("numero invalido");
-      Serial.println();
-    }
-  
-  
-  
-  
- 
-  delay(1000);
-}
+{ }
